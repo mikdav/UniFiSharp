71c0b93316e5aeee894037757b64ac07ab7cd00c
techfooninja <josh@davis.fm>
2026-07-09 16:51:13 -0500

    add GetVisitors and RemoveVisitor endpoints

--- UniFiSharp/Access/UniFiAccessApi.Visitors.cs
+++ UniFiSharp/Access/UniFiAccessApi.Visitors.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 using UniFiSharp.Access.Models;
 
@@ -11,6 +10,38 @@ namespace UniFiSharp.Access
         public async Task<Visitor> AddVisitor(Visitor visitor)
         {
             return await this.RestClient.UniFiPost<Visitor>("access/api/v2/visitor", visitor);
+        }
+
+        public async Task<List<Visitor>> GetVisitors(int batchSize = 50)
+        {
+            var visitorList = new List<Visitor>();
+            List<Visitor> tempList;
+            var pageNum = 1;
+
+            do
+            {
+                tempList = await this.RestClient.UniFiGet<List<Visitor>>($"access/api/v2/visitors?page_num={pageNum}&page_size={batchSize}");
+                visitorList.AddRange(tempList);
+                pageNum++;
+            }
+            while (tempList.Count == batchSize);
+
+            return visitorList;
+        }
+
+        public async Task RemoveVisitor(Visitor visitor)
+        {
+            if (visitor == null)
+            {
+                throw new ArgumentNullException(nameof(visitor));
+            }
+
+            if (visitor.Id == Guid.Empty)
+            {
+                throw new ArgumentNullException("visitorId");
+            }
+
+            await this.RestClient.UniFiDelete($"access/api/v2/visitor/{visitor.Id}?is_force=true");
         }
     }
 }
